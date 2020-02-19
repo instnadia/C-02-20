abce44d18915119b4ad6203969fbcb5a45f670e3
instnadia <nnguyen@codingdojo.com>
2020-02-18 16:48:54 -0800

    wedding planner

---
+++ Week 3/WeddingPlanner/Migrations/20200218223145_firstmigration.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WeddingPlanner.Migrations
+{
+    public partial class firstmigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    UserId = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    firstname = table.Column<string>(nullable: false),
+                    lastname = table.Column<string>(nullable: false),
+                    email = table.Column<string>(nullable: false),
+                    password = table.Column<string>(nullable: false),
+                    CreatedAt = table.Column<DateTime>(nullable: false),
+                    UpdatedAt = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.UserId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Weddings",
+                columns: table => new
+                {
+                    WeddingId = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Userid = table.Column<int>(nullable: false),
+                    WedderOne = table.Column<string>(nullable: false),
+                    WedderTwo = table.Column<string>(nullable: false),
+                    WeddingDate = table.Column<DateTime>(nullable: false),
+                    WeddingAddress = table.Column<string>(nullable: true),
+                    CreatedAt = table.Column<DateTime>(nullable: false),
+                    UpdatedAt = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Weddings", x => x.WeddingId);
+                    table.ForeignKey(
+                        name: "FK_Weddings_Users_Userid",
+                        column: x => x.Userid,
+                        principalTable: "Users",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Rsvp",
+                columns: table => new
+                {
+                    RsvpId = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    UserId = table.Column<int>(nullable: false),
+                    WeddingId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Rsvp", x => x.RsvpId);
+                    table.ForeignKey(
+                        name: "FK_Rsvp_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Rsvp_Weddings_WeddingId",
+                        column: x => x.WeddingId,
+                        principalTable: "Weddings",
+                        principalColumn: "WeddingId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Rsvp_UserId",
+                table: "Rsvp",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Rsvp_WeddingId",
+                table: "Rsvp",
+                column: "WeddingId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Weddings_Userid",
+                table: "Weddings",
+                column: "Userid");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Rsvp");
+
+            migrationBuilder.DropTable(
+                name: "Weddings");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
